92b00811ea79b4df87b1b8070ad26db81fc07b13
Mike Birtwistle <mikebirty90@gmail.com>
2017-04-04 00:26:17 +0100

    started albums template

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+	
+	/* Script to fix bootstrap collapsing nav bar error. Nav bar now closes after a link is clicked. */
+	
 	$(document).on('click', '.navbar-collapse.in', function(e) {
 		if( $(e.target).is('a') ) {
 		$(this).collapse('hide');
 		}
 	});
+
 });
