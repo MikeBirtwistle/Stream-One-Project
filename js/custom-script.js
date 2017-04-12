cb11345efb196b41a733094868d1176f959a5da6
Mike Birtwistle <mikebirty90@gmail.com>
2017-04-12 17:34:56 +0100

    Template and css changes

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	
-	/* Script to fix bootstrap collapsing nav bar error. Nav bar now closes after a link is clicked. */
+	/******* Script to fix bootstrap collapsing nav bar error. Nav bar now closes after a link is clicked. *******/
 	
 	$(document).on('click', '.navbar-collapse.in', function(e) {
 		if( $(e.target).is('a') ) {
