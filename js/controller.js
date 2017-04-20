b4f78d5a34ec4f021264ef1ec767677c9cb0bf71
Mike Birtwistle <mikebirty90@gmail.com>
2017-04-20 23:37:27 +0100

    Templates complete

--- js/controller.js
+++ js/controller.js
@@ -3,25 +3,49 @@ angular.module('RouteControllers', [])
 	/* Home Controller */
 
     .controller('HomeController', function($scope) {
+        
+        /* Set the title of the home page */
+
         $scope.title = "Welcome To The World Of Drake"
+
+
     })
 
     /* Enquiries Controller */
 
     .controller('EnquiriesController', function($scope) {
  
-        $scope.enquiryUser = {};
- 
-        $scope.submitForm = function() {
-            if ($scope.enquiryForm.$valid) {
-                $scope.enquiry.firstname = $scope.firstname;
-                $scope.enquiry.lastname = $scope.lastname;
-                $scope.enquiry.email = $scope.email;
-                $scope.enquiry.eventtype = $scope.eventtype;
-                $scope.enquiry.date = $scope.date;
-            }
+        /* Ensure the input fields have empty start values */
+        
+        $scope.name = "";
+        $scope.email="";
+        $scope.enquiries="";
+        
+        // make sure that error messages are not displayed until the user actually submits an invalid form:
+        
+        $scope.noNameEntered = false; 
+        $scope.noEmailEntered = false;
+        $scope.noEnquiryEntered =false;
+      
 
-            alert("Thanks, we will get back to you as soon as possible!");
+       
+        // function called when form is submitted: 
+
+        $scope.submitForm = function() {
+            /* use Angular's native form valudation to check if each individual input is OK.
+            (These are used to display specific error messages via ng-show directives in the template.) */
+            $scope.noNameEntered = !($scope.enquiryForm.name.$valid);
+            $scope.noEmailEntered = !($scope.enquiryForm.email.$valid);
+            $scope.noEnquiryEntered =!($scope.enquiryForm.enquiries.$valid);
+            
+            
+            if ($scope.enquiryForm.$valid) {
+                // if form is valid, display simple confirmation message, and clear form:
+                alert("Thank you, we aim to get back to you as soon as possible!");
                 
+                $scope.name = "";
+                $scope.email="";
+                $scope.enquiries="";
+            }       
         }
     });
